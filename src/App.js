9e73c6bbfa8187869ab32524e043739638161069
40318936 <balfaifi01@qub.ac.uk>
2022-10-06 17:51:38 +0100

    minor edites

--- src/App.js
+++ src/App.js
@@ -12,6 +12,40 @@ import FinalResult from './pages/FinalResult'
 import ErrorPage from './pages/ErrorPage';
 import Explain from './pages/Explain';
 
+
+
+//Here are the -name of actions- avalible realted to the data
+const Actions = {
+  ADD_PLAYER: "Add_Player",
+  DELETE_PLAYER: "Delete_Player",
+};
+
+// Here are the switching to the implemantation of each action
+function reducer(state, action) {
+  switch (action.type) {
+    case Actions.ADD_PLAYER:
+      return [...state, ...newPlayer(action.payload.names)];
+    case Actions.DELETE_PLAYER:
+      return state;
+    default:
+      throw new Error();
+  }
+}
+
+const newPlayer = (names) => {
+  console.log(names.length);
+  let obj = [];
+  for (let i = 0; i < names.length; i++) {
+    obj[i] = {
+      name: names[i],
+      score: 0
+    };
+  }
+  return obj;
+};
+
+
+
 function App() {
   return (
     <div className="App">
